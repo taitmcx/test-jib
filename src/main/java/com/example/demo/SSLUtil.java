bab3dba5f76f33f4ba84df51afc96fe4800195af
taitmcx <ahmetcabaoglu@outlook.com>
2024-06-25 22:48:10 +0300

    .

---
+++ src/main/java/com/example/demo/SSLUtil.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import javax.net.ssl.*;
+import java.security.cert.X509Certificate;
+
+public class SSLUtil {
+
+    public static void disableSSLVerification() {
+        try {
+            TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return null;
+                        }
+
+                        public void checkClientTrusted(X509Certificate[] certs, String authType) {
+                        }
+
+                        public void checkServerTrusted(X509Certificate[] certs, String authType) {
+                        }
+                    }
+            };
+
+            SSLContext sc = SSLContext.getInstance("SSL");
+            sc.init(null, trustAllCerts, new java.security.SecureRandom());
+            HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+
+            HostnameVerifier allHostsValid = (hostname, session) -> true;
+            HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
